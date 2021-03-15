7db516651803d40c9d47c6d27cb7e39f831c2533
kevinsherrell <k.sherrell.1028@gmail.com>
2021-03-14 18:27:54 -0600

    ..

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,44 +1,88 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
+import '../app.css';
 
 class GameContainer extends Component {
     state = {
-        question: null,
-        score: null,
-        error: null,
         answerRevealed: false,
+        score: 0
     }
 
 
-    componentDidMount =()=>{
+    getQuestion = () => {
         // get 1 random question
-        Axios.get('http://jservice.io/api/random?count=1')
+        Axios.get('http://jservice.io/api/random')
             .then(question => {
                 this.setState({
-                    question: question.data[0]
-                },()=>console.log(this.state.question));
+                    ...question.data[0]
+                }, () => console.log(this.state));
             })
             .catch(err => {
-            console.log(err);
-            this.setState({
-                error: err
-            },()=>console.log(this.state));
+                this.setState({
+                    error: err
+                }, () => console.log(this.state.error));
+            })
+    }
+    revealAnswer = () => {
+        console.log('revealanswer')
+        this.setState({
+            answerRevealed: !this.state.answerRevealed
         })
     }
+    increaseScore = () => {
+        if (this.state.question) {
+            this.setState({
+                score: this.state.score += this.state.value,
+            });
+        }
+
+    }
+    decreaseScore = () => {
+        if (this.state.question) {
+            this.setState({
+                score: this.state.score -= this.state.value,
+            });
+        }
+
+    }
+    resetScore = () => {
+        this.setState({
+            score: 0
+        })
+    };
 
     render() {
-console.log(this.state)
+        console.log(this.state.question)
         return (
             <div>
-                <button>Decrease</button>
-                <button>Increase</button>
-                <button>Reset</button>
-                {/*<p className="category">{category.title}</p>*/}
-                {/*<p className="question">{this.state.question.question}</p>*/}
-                <p className="answer">{this.state.question.answer}</p>
+                <div className={'scoreContainer'}>
+                    <h2>Score: {this.state.score}</h2>
+                    <button onClick={this.decreaseScore}>Decrease</button>
+                    <button onClick={this.increaseScore}>Increase</button>
+                    <button onClick={this.resetScore}>Reset</button>
+                </div>
+                <div className={'getQuestion'}>
+                    <h1>Let's Play!</h1>
+                    <button onClick={this.getQuestion}>Get Question</button>
+                </div>
+
+                {this.state.question &&
+                <>
+                    <p className="category">Category: {this.state.category && this.state.category.title}</p>
+                    <p className="question">Question: {this.state.question && this.state.question}state</p>
+                </>
+                }
+                <div className="revealAnswer">
+                    <button onClick={this.revealAnswer}>reveal answer!</button>
+                    {this.state.answerRevealed &&
+                    <p className={'answer'}>Answer: {this.state.answer && this.state.answer}</p>
+                    }
+                </div>
+
             </div>
         )
     }
+
 }
 
 export default GameContainer;
